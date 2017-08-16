63628da33eae45604c2852aef5fb3e14ab55fb0c
Nikola Brežnjak <nikola.breznjak@gmail.com>
2017-08-16 09:26:29 +0200

    Finished project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,24 +7,27 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { AdMobPro } from '@ionic-native/admob-pro';
+
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        HomePage
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        AdMobPro,
+        { provide: ErrorHandler, useClass: IonicErrorHandler }
+    ]
 })
-export class AppModule {}
+export class AppModule { }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AdMobPro } from '@ionic-native/admob-pro';
+import { Platform } from 'ionic-angular';
+
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
+    constructor(public navCtrl: NavController, platform: Platform, private admob: AdMobPro) {
+        platform.ready().then(() => {
+            var admobid = {
+                banner: 'ca-app-pub-7957971173858308/5068937357',
+                interstitial: 'ca-app-pub-7957971173858308/5667703151'
+            };
 
-  constructor(public navCtrl: NavController) {
+            this.admob.createBanner({
+                adId: admobid.banner,
+                isTesting: true,
+                autoShow: true,
+                position: this.admob.AD_POSITION.BOTTOM_CENTER
+            })
 
-  }
+            this.admob.prepareInterstitial({
+                adId: admobid.interstitial,
+                isTesting: true,
+                autoShow: false
+            })
+        });
+    }
 
+    showInterstitialAd() {
+        if (AdMobPro) {
+            this.admob.showInterstitial();
+        }
+    }
 }
